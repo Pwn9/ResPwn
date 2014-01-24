2e5783588f5e36c3a9f76d0f92e094d4d81f896b
tremor <admin@pwn9.com>
2014-01-24 00:35:10 -0500

    Bits for armor slots and enchantments.
    In progress, added armor slot, enchantment bits. To-do, lore, value
    checks, importing config.yml values and developing a simple system for
    doing the config for respwn armor.

--- src/com/pwn9/ResPwn/Inventory.java
+++ src/com/pwn9/ResPwn/Inventory.java
@@ -1,7 +1,11 @@
 package com.pwn9.ResPwn;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -12,27 +16,55 @@ public class Inventory
 	// We'll set inventories here
 	public static void ResInventory(Player p, World w) 
 	{
+		// Before we do anything, should check permissions
+		
+		
+		// And should check the configs
+		
+		
+		// Get players inventory - it should be empty (ish)
+		PlayerInventory pi = p.getInventory();
+		
 		
 	}
 	
 	// We'll set armors here
 	public static void ResArmor(Player p, World w) 
 	{
-
-		//example
+		// Before we do anything, should check permissions
 		
+		
+		// And should check the configs
+		
+		
+		// Get players inventory - it should be empty (ish)
 		PlayerInventory pi = p.getInventory();
 		
+		// Helmet
 		ItemStack gethelmet = new ItemStack(Material.IRON_HELMET);
+		Map<Enchantment, Integer> helmetEnchants = new HashMap<Enchantment, Integer>();
+		helmetEnchants.put(Enchantment.PROTECTION_ENVIRONMENTAL, 3);
+		gethelmet.addEnchantments(helmetEnchants);
+		
+		// Boots
 		ItemStack getboots = new ItemStack(Material.IRON_BOOTS);
 		
-		ItemStack getsword = new ItemStack(Material.IRON_SWORD);
+		// Leggings (I'm calling them pants)
+		ItemStack getpants = new ItemStack(Material.IRON_LEGGINGS);
 		
+		// Chestplate
+		ItemStack getplate = new ItemStack(Material.IRON_CHESTPLATE);
+		
+		// Item in hand (I'm calling it wield)
+		ItemStack getwield = new ItemStack(Material.IRON_SWORD);
+		
+		
+		// Give player the items
 		pi.setHelmet(gethelmet);
 		pi.setBoots(getboots);
-		
-		pi.setItemInHand(getsword);
-		
+		pi.setLeggings(getpants);
+		pi.setChestplate(getplate);
+		pi.setItemInHand(getwield);	
 		 
 	}
 	
